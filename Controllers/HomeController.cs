152c164161ce7dfd925717030e81ed3d29ae1fc5
Daniel031722 <daniel031722@gmail.com>
2025-07-17 23:35:28 +0530

    Initial commit with backend code

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskApi.Controllers
+{
+    [ApiController]
+    [Route("home")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("getHealth")]
+        public IActionResult GetHealth()
+        {
+            return Ok("API is healthy ✅");
+        }
+    }
+}
