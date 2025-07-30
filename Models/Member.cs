fc787b9037cf10d2aec5b4b35bb1df068e0e0ffb
Daniel031722 <daniel031722@gmail.com>
2025-07-30 16:17:51 +0530

    Update Member.cs

--- Models/Member.cs
+++ Models/Member.cs
@@ -3,6 +3,8 @@ namespace personal_task_tracker_backend.Models
     public class Member
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string College { get; set; } = string.Empty;
     }
 }
