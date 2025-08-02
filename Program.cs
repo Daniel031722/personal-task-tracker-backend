42a6c192c00e3b758f54cf584a4fbf66c2ca0681
Khushi-chitari <khushichitari6@gmail.com>
2025-08-02 15:52:26 +0530

    Added CORS policy configuration in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,55 @@
+// using Microsoft.EntityFrameworkCore;
+// using TaskApi.Data;
+
+// var builder = WebApplication.CreateBuilder(args);
+
+// // Add services
+// builder.Services.AddControllers();
+// builder.Services.AddDbContext<TaskDbContext>(options =>
+//     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
+
+// var app = builder.Build();
+
+// // Configure app
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+// app.UseHttpsRedirection();
+// app.UseAuthorization();
+// app.MapControllers();
+// app.Run();
+
 using Microsoft.EntityFrameworkCore;
 using TaskApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services
+// 1. Add services
 builder.Services.AddControllers();
 builder.Services.AddDbContext<TaskDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// 👉 Add CORS policy here
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontendApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000") // 👈 Add your React frontend URL here
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
-// Configure app
+// 2. Configure app
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -20,6 +57,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// 👉 Use CORS before Authorization
+app.UseCors("AllowFrontendApp");
+
 app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
